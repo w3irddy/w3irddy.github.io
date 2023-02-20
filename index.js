237cd81fc3870b02bc965449ea1132aff2765fb0
w3irddy <weirddd42@gmail.com>
2023-02-20 17:03:46 +0300

    added javascript

---
+++ index.js
@@ -0,0 +1,72 @@
+function _0x220e() {
+    const _0x197830 = [
+        'style',
+        '2547CJziUS',
+        'blue',
+        'youtube',
+        'addEventListener',
+        '9784cCAtyb',
+        '5XbnYQv',
+        '5ARMjWt',
+        'mouseover',
+        '5572968hwmaCb',
+        '748742ApYUHi',
+        'borderColor',
+        '1159720dGBpfb',
+        'getElementById',
+        'mouseout',
+        'github',
+        '1964832KCaHOf',
+        'discord',
+        '10668954JAvrqH',
+        'red',
+        '3742816hDcJKS'
+    ];
+    _0x220e = function () {
+        return _0x197830;
+    };
+    return _0x220e();
+}
+function _0x8af7(_0xbe9809, _0x2488fd) {
+    const _0x220e6e = _0x220e();
+    return _0x8af7 = function (_0x8af754, _0x244f7e) {
+        _0x8af754 = _0x8af754 - 0x1e7;
+        let _0x5b903c = _0x220e6e[_0x8af754];
+        return _0x5b903c;
+    }, _0x8af7(_0xbe9809, _0x2488fd);
+}
+const _0x56f368 = _0x8af7;
+(function (_0x432a6c, _0x425a93) {
+    const _0x2ac729 = _0x8af7, _0x2c38a8 = _0x432a6c();
+    while (!![]) {
+        try {
+            const _0x19c043 = parseInt(_0x2ac729(0x1e7)) / 0x1 * (-parseInt(_0x2ac729(0x1eb)) / 0x2) + -parseInt(_0x2ac729(0x1f1)) / 0x3 + parseInt(_0x2ac729(0x1ea)) / 0x4 + -parseInt(_0x2ac729(0x1e8)) / 0x5 * (-parseInt(_0x2ac729(0x1f3)) / 0x6) + parseInt(_0x2ac729(0x1f5)) / 0x7 + -parseInt(_0x2ac729(0x1fb)) / 0x8 * (parseInt(_0x2ac729(0x1f7)) / 0x9) + parseInt(_0x2ac729(0x1ed)) / 0xa;
+            if (_0x19c043 === _0x425a93)
+                break;
+            else
+                _0x2c38a8['push'](_0x2c38a8['shift']());
+        } catch (_0x539c29) {
+            _0x2c38a8['push'](_0x2c38a8['shift']());
+        }
+    }
+}(_0x220e, 0xe7ba1));
+const github = document[_0x56f368(0x1ee)](_0x56f368(0x1f0)), discord = document[_0x56f368(0x1ee)](_0x56f368(0x1f2)), youtube = document[_0x56f368(0x1ee)](_0x56f368(0x1f9));
+github[_0x56f368(0x1fa)](_0x56f368(0x1e9), () => {
+    const _0x3e685e = _0x56f368;
+    github[_0x3e685e(0x1f6)]['borderColor'] = _0x3e685e(0x1f4);
+}), discord[_0x56f368(0x1fa)](_0x56f368(0x1e9), () => {
+    const _0x3628d6 = _0x56f368;
+    discord[_0x3628d6(0x1f6)][_0x3628d6(0x1ec)] = 'red';
+}), youtube[_0x56f368(0x1fa)](_0x56f368(0x1e9), () => {
+    const _0x129408 = _0x56f368;
+    youtube[_0x129408(0x1f6)]['borderColor'] = _0x129408(0x1f4);
+}), github['addEventListener'](_0x56f368(0x1ef), () => {
+    const _0x32da8e = _0x56f368;
+    github['style'][_0x32da8e(0x1ec)] = _0x32da8e(0x1f8);
+}), discord[_0x56f368(0x1fa)](_0x56f368(0x1ef), () => {
+    const _0x1a98dd = _0x56f368;
+    discord[_0x1a98dd(0x1f6)]['borderColor'] = _0x1a98dd(0x1f8);
+}), youtube[_0x56f368(0x1fa)]('mouseout', () => {
+    const _0x341946 = _0x56f368;
+    youtube[_0x341946(0x1f6)][_0x341946(0x1ec)] = _0x341946(0x1f8);
+});
